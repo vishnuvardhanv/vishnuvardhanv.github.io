9b9e6e58941f3df3d16ab8849e2347f214bbcbdb
Vishnu Vardhan V <Vishnu Vardhan V>
2017-01-05 03:22:49 +0530

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,77 @@
+window.Instagram = {
+    /**
+     * Store application settings
+     */
+    config: {},
+
+    BASE_URL: 'https://api.instagram.com/v1',
+
+    init: function( opt ) {
+        opt = opt || {};
+
+        this.config.client_id = opt.client_id;
+        this.config.access_token = opt.access_token;
+    },
+
+    /**
+     * Get a list of popular media.
+     */
+    recent: function( callback ) {
+        var endpoint = this.BASE_URL + '/users/self/media/recent/?access_token=' + this.config.access_token;
+        console.log(endpoint);
+        this.getJSON( endpoint, callback );
+    },
+
+    /**
+     * Get a list of recently tagged media.
+     */
+    tagsByName: function( name, callback ) {
+        var endpoint = this.BASE_URL + '/tags/' + name + '/media/recent?access_token=' + this.config.access_token;
+        this.getJSON( endpoint, callback );
+    },
+
+    getJSON: function( url, callback ) {
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'jsonp',
+            success: function( response ) {
+                if ( typeof callback === 'function' ) callback( response );
+            }
+        });
+    }
+};
+
+Instagram.init({
+    client_id: 'aa9fe693cffa4434b753da6c631a9fa2',
+    access_token: '4175543450.aa9fe69.1364c8d46e1b4a09bff9ab3097401012'
+});
+
+
+$( document ).ready(function() {
+
+    Instagram.recent(function( response ) {
+        var $instagram = $( '#instagram' );
+        for ( var i = 0; i < response.data.length; i++ ) {
+            imageUrl = response.data[i].images.low_resolution.url;
+            $instagram.append( '<img src="' + imageUrl + '" />' );
+        }
+    });
+
+    $( '#form' ).on('submit', function( e ) {
+        e.preventDefault();
+
+        var tagName = $( '#search' ).val();
+        Instagram.tagsByName(tagName, function( response ) {
+            var $instagram = $( '#instagram' );
+                $instagram.html('');
+
+            for ( var i = 0; i < response.data.length; i++ ) {
+                imageUrl = response.data[i].images.low_resolution.url;
+                $instagram.append( '<img src="' + imageUrl + '" />' );
+            }
+        });
+
+    });
+
+});
