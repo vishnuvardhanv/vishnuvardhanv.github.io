dc9a0934cb05cf91f9f63c53f3e0ab486619cdbe
Tamilselvan T <tamilselvant@Tamilselvans-MacBook-Pro.local>
2019-06-17 23:01:00 +0530

    new code added

---
+++ precache-manifest.6a49b62a5d2570e505fdfc61a0a3566b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe217598499b4e99dde28217d61e396e",
+    "url": "/img/bgc-team.fe217598.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7101404f04a05ed406bfc0fd433ffb0d",
+    "url": "/img/fun-land-slider.7101404f.jpg"
+  },
+  {
+    "revision": "b3e33eaf48df8ba890ea",
+    "url": "/js/chunk-vendors.bac69608.js"
+  },
+  {
+    "revision": "be8e6cef88bab107db97a29685e19b13",
+    "url": "/img/automotive-home-image.be8e6cef.jpg"
+  },
+  {
+    "revision": "303bfde9a062035857c8a7535ef68ef1",
+    "url": "/img/bgc-slider-home.303bfde9.jpg"
+  },
+  {
+    "revision": "4d4506dd0778d4fd52daf11c13b2f09b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c85f657f057e4fb5fbece3cbd3b6bd9f",
+    "url": "/img/careers-header.c85f657f.jpg"
+  },
+  {
+    "revision": "2177d56d06fdf7ba386a",
+    "url": "/js/app.18a31e7c.js"
+  },
+  {
+    "revision": "6233daab7a90055277d2b7b1a120e9e3",
+    "url": "/img/hijaz-mall.6233daab.jpg"
+  },
+  {
+    "revision": "1d50c1ffefd2a2245e171cffe4f33910",
+    "url": "/img/industrial-home.1d50c1ff.jpg"
+  },
+  {
+    "revision": "2e4b8c9bd8e52c1f915d4f2d9a475c0e",
+    "url": "/img/financial-services-home.2e4b8c9b.jpg"
+  },
+  {
+    "revision": "c39845082c2f6bfa7782866ea95e5632",
+    "url": "/img/real-estate-home.c3984508.jpg"
+  },
+  {
+    "revision": "6419d17dc605583a965044d122a9af5e",
+    "url": "/img/balubaid-logo-web-2018-1.6419d17d.png"
+  },
+  {
+    "revision": "d4ddb4faafb2bb330d4e82e04445e238",
+    "url": "/img/affliates-home.d4ddb4fa.jpg"
+  },
+  {
+    "revision": "2177d56d06fdf7ba386a",
+    "url": "/css/app.9bae6cf3.css"
+  }
+];
